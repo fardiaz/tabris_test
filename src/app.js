bf7543a48fa3549ab6db17cf789204f23ef473cf
tege <fardiazmail@yahoo.com>
2017-06-28 09:39:44 +0700

    add admob plugin

--- src/app.js
+++ src/app.js
@@ -12,4 +12,27 @@ let textView = new TextView({
 
 button.on('select', () => {
   textView.text = 'Tabris.js rocks!';
+
+  AdMob.createBanner( {                                                                                                                 
+          adId: "ca-app-pub-1757158960646404/2448352574",
+          isTesting: true,
+          overlap: false,
+          offsetTopBar: false,
+          adSize: 'SMART_BANNER',
+          position: AdMob.AD_POSITION.BOTTOM_CENTER,
+          bgColor: 'black',
+          autoShow:true,
+          success: function(){
+              //alert('success to create banner');
+              textView.text = 'success to create banner';
+          },
+          error: function(){
+              //alert('failed to create banner');
+              textView.text = 'failed to create banner';
+          }
+
+        } );
+
+  });
+
 });
